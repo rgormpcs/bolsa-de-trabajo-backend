c39d0622f3ddafb9027efdccdaff17513ee66dfe
Cristian <crissmtl@gmail.com>
2016-07-27 18:17:42 -0500

    crar funcion login

--- api/controllers/EmpleadorController.js
+++ api/controllers/EmpleadorController.js
@@ -6,6 +6,23 @@
  */
 
 module.exports = {
-	
-};
+  login: function(req, res) {
+    var parametros = req.allParams();
+    console.log(parametros);
+    Empleador.findOne({
+      correoEmpleador: parametros.correo
+    }).exec(function(err, empleador) {
 
+      if (err) console.log(err);
+
+      if (parametros.contrasenia != empleador.contraseniaEmpleador) {
+        return res.badRequest({
+          error: 'Contraseña Incorrecto'
+        });
+      } else {
+        delete empleador.contraseniaEmpleador;
+        return res.ok(empleador);
+      }
+    });
+  }
+};

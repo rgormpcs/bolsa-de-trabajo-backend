a88eaa786fa86e680dbfcf1fe04fd23d18e1ba21
hdcrldu55 <hdcrldu55@gmail.com>
2016-08-05 12:20:27 -0500

    editar postulante

--- api/controllers/PostulanteController.js
+++ api/controllers/PostulanteController.js
@@ -24,5 +24,42 @@ module.exports = {
         return res.ok(postulante);
       }
     });
+  },
+  getPostulante: function(req, res) {
+    var parametros = req.allParams();
+    console.log(parametros);
+    Postulante.findOne({
+      id: parametros.idPostulante
+    }).exec(function(err, postulante) {
+      if (err) console.log(err);
+      return res.ok(postulante);
+    });
+  },
+  updatePostulante: function(req, res) {
+    var parametros = req.allParams();
+    console.log(parametros);
+    Postulante.update({
+      id: parametros.idPostulante
+    }, {
+      cedulaPostulante: parametros.cedulaPostulante,
+      nombrePostulante: parametros.nombrePostulante,
+      apellidoPostulante: parametros.apellidoPostulante,
+      telefonoPostulante: parametros.telefonoPostulante,
+      celularPostulante: parametros.celularPostulante,
+      emailPostulante: parametros.emailPostulante,
+      nacionalidadPostulante: parametros.nacionalidadPostulante,
+      estadoCivilPostulante: parametros.estadoCivilPostulante,
+      generoPostulante: parametros.generoPostulante,
+      fechaNacimientoPostulante: parametros.fechaNacimientoPostulante,
+      descripcionPerfilPostulante: parametros.descripcionPerfilPostulante,
+      cvPostulante: parametros.cvPostulante,
+      contraseniaPostulante: parametros.contraseniaPostulante,
+
+
+    }).exec(function(err, postulante) {
+      if (err) console.log(err);
+      console.log('postulante updated', postulante);
+      return res.ok(postulante[0]);
+    });
   }
 };
